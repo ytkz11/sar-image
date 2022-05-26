ed8fa23ed5fc5018cb39b6b8cc2cdc438aaf5840
ytkz11 <ytkz11@163.com>
2022-05-26 10:51:22 +0800

    Auto-committed on 20220526_周四_105122_67

--- git_update.bat
+++ git_update.bat
@@ -1,5 +1,7 @@
 @echo off
-set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+set mydate=%date:/=%
+set mytime=%time::=%
+set mytimestamp=%mydate: =_%_%mytime:.=_%
 git add .
-git commit -m"Auto-committed on %datetimef%"&&git push origin master
+git commit -m"Auto-committed on %mytimestamp%"&&git push origin master
 echo. & pause
