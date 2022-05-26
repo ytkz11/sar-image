beebbf63e444ee68e990d703bda4538abdb9752e
ytkz11 <ytkz11@163.com>
2022-05-26 10:48:41 +0800

    Auto-committed on 6 周四_2/_20__10_48_40

--- git_update.bat
+++ git_update.bat
@@ -1,4 +1,5 @@
 @echo off
+set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
 git add .
-git commit -m"Auto-committed on %date% %time%"&&git push origin master
+git commit -m"Auto-committed on %datetimef%"&&git push origin master
 echo. & pause
